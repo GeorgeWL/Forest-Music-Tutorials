96d42ebf0dbdc5bb0a9f1fc36c83a43e4ef6b6ae
LAPTOP-SG44TT74\george <georgelangham1990@gmail.com>
2017-02-25 11:09:56 +0000

    LiepzigMusicService and LiepzigMusicController
    Some weirdness as I created the folder strcture wrongat first then had
    to modify and rebuild liepzig.services in order for VS2013 to notice the
    changes.

---
+++ Forest/Controllers/LiepzigMusicController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Liepzig.Services.IService;
+using Liepzig.Services;
+using Liepzig.Services.Service;
+
+namespace Forest.Controllers
+{
+    public class LiepzigMusicController : Controller
+    {
+        private Liepzig.Services.Service.LiepzigMusicService _LiepzigMusicService;
+        public LiepzigMusicController()
+        {
+            _LiepzigMusicService = new Liepzig.Services.Service.LiepzigMusicService();
+        }
+        //
+        // GET: /LiepzigMusic/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ Liepzig.Services/Service/LiepzigMusicService.cs
@@ -0,0 +1,24 @@
+﻿using Liepzig.Services.IService;
+using Liepzig.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liepzig.Services.Service
+{
+   public class LiepzigMusicService : ILiepzigMusicService
+        {
+            private LiepzigServices.ForestWebService _proxy;
+            public LiepzigMusicService()
+            {
+                _proxy = new LiepzigServices.ForestWebService();
+            }
+
+            public IList<LiepzigServices.MusicBEAN> GetLiepzigMusicRecordings(int genre)
+            {
+                return _proxy.GetMusicRecordings(genre);
+            }
+        }
+}
